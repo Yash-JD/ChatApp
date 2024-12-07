c0d8f7c94fc148587b202d36eab9d7c5f7771afe
Yash <yjdobariya0@gmail.com>
2024-12-07 13:40:56 +0530

    basic setup done

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+
+dotenv.config(); // with this all the environment variable will be inside process.env
+
+const app = express();
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL;
+
+// cors is used to communicate between front-end & back-end
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+}));
+
+const server = app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+    
+});
+
+mongoose.connect(databaseURL)
+    .then(() => console.log('Database connnectd'))
+    .catch(err=>console.log(err.message))
+    
